c1c06444452021feb606a3a60ef28035ac04befd
Jobin Nelson <jobinnelson369@gmail.com>
2022-10-15 13:27:51 +0530

    finished calculator

--- src/calculator.rs
+++ src/calculator.rs
@@ -53,14 +53,92 @@ impl Calculator {
                         return Err(Error::MismatchedParens);
                     }
                 }
-                _ => {}
+                '+' => tokens.push(Token::Op(Operator::Add)),
+                '-' => tokens.push(Token::Op(Operator::Sub)),
+                '*' => tokens.push(Token::Op(Operator::Mul)),
+                '/' => tokens.push(Token::Op(Operator::Div)),
+                ' ' | '\n' => {}
+                _ => return Err(Error::BadToken(c)),
             }
         }
+
+        if parens.len() > 0 {
+            return Err(Error::MismatchedParens);
+        }
+
         Ok(tokens)
     }
     
     pub fn expression(mut tokens: Vec<Token>) -> Vec<Token> {
-        
+        tokens.reverse();
+
+        let mut queue: Vec<Token> = Vec::new(); 
+        let mut stack: Vec<Token> = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            match token {
+                Token::Number(_) => queue.push(token),
+                Token::Op(_) => {
+                    while !stack.is_empty() && stack[stack.len() - 1] >= token {
+                        queue.push(stack.pop().unwrap());
+                    }
+                    stack.push(token);
+                }
+                Token::Bracket('(') => stack.push(token),
+                Token::Bracket(')') => {
+                    while !stack.is_empty() && stack[stack.len() - 1] != Token::Bracket('(') {
+                        queue.push(stack.pop().unwrap());
+                    }
+                    stack.pop();
+                }
+                _ => {}
+            }
+        }
+
+        while stack.len() > 0 {
+            queue.push(stack.pop().unwrap());
+        }
+
+        queue
+    }
+
+    pub fn evaluate(mut tokens: Vec<Token>) -> Option<f32> {
+        tokens.reverse();
+
+        let mut stack: Vec<f32> = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            match token {
+                Token::Number(num) => stack.push(num as f32),
+                Token::Op(Operator::Add) => {
+                    let right = stack.pop().unwrap();
+                    let left = stack.pop().unwrap();
+                    stack.push(left + right);
+                }
+                Token::Op(Operator::Sub) => {
+                    let right = stack.pop().unwrap();
+                    let left = stack.pop().unwrap();
+                    stack.push(left - right);
+                }
+                Token::Op(Operator::Mul) => {
+                    let right = stack.pop().unwrap();
+                    let left = stack.pop().unwrap();
+                    stack.push(left * right);
+                }
+                Token::Op(Operator::Div) => {
+                    let right = stack.pop().unwrap();
+                    let left = stack.pop().unwrap();
+                    stack.push(left / right);
+                }
+                _ => {}
+            }
+        }
+
+        if stack.len() > 1 {
+            None
+        } else {
+            stack.pop()
+        }
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@ use calculator::{Calculator, Error};
 
 mod calculator;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Error> {
+    loop {
+        let mut input = String::new();
+
+        match std::io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let tokens = Calculator::parse(input);
+                if tokens.is_err() {
+                    eprintln!("{:?}", tokens.err().unwrap());
+                    continue;
+                }
+                let expr = Calculator::expression(tokens?);
+                if let Some(v) = Calculator::evaluate(expr) {
+                    println!("{}", v);
+                }
+            }
+            Err(error) => eprintln!("error: {}", error),
+        }
+    }
 }
